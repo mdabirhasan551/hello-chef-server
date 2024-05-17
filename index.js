ac5e1404af2a39e3069df4bcb13c26dbab0112ce
dev_abir <59115184+mdabirhasan551@users.noreply.github.com>
2024-05-18 05:30:43 +0600

    chef and recipe data added in separate json files

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const app = express();
 const cors = require("cors");
 const port = 5000;
 
-const data = require('./data/data.json')
-
+const data = require("./data/data.json");
 
 app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("Hello from server 300");
+  res.send(data);
 });
 
-app.get("/data", (req, res) => {
-    res.send(data);
-  });
 
-
+app.get('/chef/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedChef = data.find(c => c._id === id);
+  console.log(selectedChef);
+  res.send(selectedChef)
+})
 
 app.listen(port, () => {
   console.log("server is running on port: ", port);
