dcb81d705c69f5cc43287f7d1aa6af34fe9e2606
dev_abir <59115184+mdabirhasan551@users.noreply.github.com>
2024-05-17 20:57:38 +0600

    data loaded and running on port

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = 5000;
 
-app.get ('/', (req,res)=>{
-    res.send('Hello from server 300');
-})
+const data = require('./data/data.json')
 
-app.listen(port,()=>{
-    console.log('server is running on port: ', port);
-})
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Hello from server 300");
+});
+
+app.get("/data", (req, res) => {
+    res.send(data);
+  });
+
+
+
+app.listen(port, () => {
+  console.log("server is running on port: ", port);
+});
