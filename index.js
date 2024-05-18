d2a49b1b5eddf557a12f952dd46a43804be901c5
dev_abir <59115184+mdabirhasan551@users.noreply.github.com>
2024-05-18 18:42:48 +0600

    environment variable setted

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 5000;
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const port = process.env.PORT || 5000;
 
 const data = require("./data/data.json");
 const recipes = require("./data/recipes.json");
