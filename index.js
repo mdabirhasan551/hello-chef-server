38be4b5164e7f78edc652555e56203abbc30060a
dev_abir <59115184+mdabirhasan551@users.noreply.github.com>
2024-05-18 12:14:16 +0600

    chef.json updated

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const port = 5000;
 
 const data = require("./data/data.json");
+const recipes = require("./data/recipes.json");
+const chef = require("./data/chef.json");
 
 app.use(cors());
 
@@ -12,13 +14,26 @@ app.get("/", (req, res) => {
 });
 
 
-app.get('/chef/:id', (req, res) => {
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
+
+
+app.get("/chef", (req, res) => {
+  res.send(chef);
+});
+
+app.get("/chef/:id", (req, res) => {
   const id = req.params.id;
-  console.log(id);
-  const selectedChef = data.find(c => c._id === id);
-  console.log(selectedChef);
-  res.send(selectedChef)
-})
+  const selectedChef = data.find((c) => c._id == id);
+  res.send(selectedChef);
+});
+
+app.get("/recipes/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedRecipe = recipes.find((r) => r.id == id);
+  res.send(selectedRecipe);
+});
 
 app.listen(port, () => {
   console.log("server is running on port: ", port);
